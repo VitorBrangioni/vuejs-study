748ee40644b653bbc04891cf10de6b6ff66a3aa4
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-07-03 15:59:27 -0300

    Creating routes with Single Page Application

--- brangi-pics/src/App.vue
+++ brangi-pics/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
+    <meu-menu :routes="routes" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
+import { routes } from './routes.js';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
+
+  components: {
+    'meu-menu': Menu
+  },
+
+  data() {
+    
+    return {
+      routes
+    }
+  }
+
+  // passar para template
+
 }
 
 </script>
